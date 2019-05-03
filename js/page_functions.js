d1baf07974163fc7088a829a4838df4be8d250c2
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-05-02 21:23:37 -0500

    added more styling and stuff

---
+++ js/page_functions.js
@@ -0,0 +1,7 @@
+$("#set_1").on("click", function () {
+	// load solutions that have been workied on as forms
+	// where the input is the number and the output is
+	// the result of the calculations. Also load the
+	// problem criteria as listed in the PE chellenge
+	// itself.
+})
